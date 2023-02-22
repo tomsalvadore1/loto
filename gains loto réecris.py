3157583e3cb18895e1967f684ce48c76cfa15e19
tomsalvadore1 <126028428+tomsalvadore1@users.noreply.github.com>
2023-02-22 17:58:56 +0100

    Add files via upload

--- gains loto réecris.py
+++ gains loto réecris.py
@@ -1,8 +1,8 @@
 
 import random
 
-class grille:
-    def __init__(self):
+class grille: #la personne remplit sa grille 
+    def __init__(self):#5 numeros chois plus le complementaire 
         self.numero1 = None
         self.numero2 = None
         self.numero3 = None
@@ -12,31 +12,31 @@ class grille:
         self.numero = [0] * 5
 
     def choisistesnumeros(self):
-        def verifnumero(num, a, b):
+        def verifnumero(num, a, b):#methode pour verifier si un numero est bien compris entre 2 bornes (pour les 5 premiers 0:50, pour le complementaire 0:10)
             while num <= a or num > b:
                 print("choisis un nombre valide")
-                num = int(input())
+                num = int(input())#si c'est pas valide, la personne rechoisis 
             return num
 
-        def pasdeduplicata(self):
+        def pasdeduplicata(self):#methode pour verifier que chaque nombre choisis est unique 
             self.numero = [self.numero1, self.numero2, self.numero3, self.numero4, self.numero5]
-            if len(set(self.numero)) != len(self.numero):
+            if len(set(self.numero)) != len(self.numero):#les sets ne permettent pas de duplicata alors lors de la conversion les duplicatas sont supprimes 
                 return False
             else:
                 return True
 
         while pasdeduplicata(self) == False:
-            self.numero1 = verifnumero(int(input("chois ton premier numero ")), 0, 50)
+            self.numero1 = verifnumero(int(input("chois ton premier numero ")), 0, 50)#on applique les verifications a tous les nombres 
             self.numero2 = verifnumero(int(input("chois ton second numero ")), 0, 50)
             self.numero3 = verifnumero(int(input("chois ton troisieme numero ")), 0, 50)
             self.numero4 = verifnumero(int(input("chois ton quatrieme numero ")), 0, 50)
             self.numero5 = verifnumero(int(input("chois ton cinquieme numero ")), 0, 50)
             self.numerocomp = verifnumero(int(input("chois ton numero complémentaire ")), 0, 9)
-        return [self.numero1,self.numero2,self.numero3,self.numero4,self.numero5,self.numerocomp]
+        return [self.numero1,self.numero2,self.numero3,self.numero4,self.numero5,self.numerocomp]#on renvoie une liste avec les numeros choisis 
 
 
 class francaisedesjeux: 
-    def __init__(self):
+    def __init__(self):#les numeros gagnants von etre choisis 
         self.grille = list(range(50))
         self.gagnant1 = None
         self.gagnant2 = None
@@ -49,8 +49,8 @@ class francaisedesjeux:
 
 
         def tirage(self):
-            gagnant = random.choice(self.grille) # choice c'est trouve un numéro dans la liste donnée apres 
-            self.grille.remove(gagnant)
+            gagnant = random.choice(self.grille) #on choisis un nulero dans la grille de 0 a 5 au hasard 
+            self.grille.remove(gagnant)#on le supprime de la grille pour s'assurer de ne pas avoir de doubles 
             return gagnant
 
         self.gagnant1 = tirage(self)
@@ -58,21 +58,21 @@ class francaisedesjeux:
         self.gagnant3 = tirage(self)
         self.gagnant4 = tirage(self)
         self.gagnant5 = tirage(self)
-        self.gagnantcomp = random.choice(list(range(9)))
-        return [self.gagnant1,self.gagnant2,self.gagnant3,self.gagnant4,self.gagnant5,self.gagnantcomp]
+        self.gagnantcomp = random.choice(list(range(9)))#le numero complementaire c'est particulier 
+        return [self.gagnant1,self.gagnant2,self.gagnant3,self.gagnant4,self.gagnant5,self.gagnantcomp]#on renvoie une liste avec les numeros gagnatns 
 
 
 class verif:
     def __init__(self):
         self.nombregaganant = 0
 
-    def cinqpremiers(self,grille1,grille2):
+    def cinqpremiers(self,grille1,grille2):#on obtient le nombre de numeros gagnants 
         for i in grille1[0:5]:
             self.nombregaganant += grille2[0:5].count(i)
         print(self.nombregaganant)
         return self.nombregaganant
 
-    def comp(self,grille1,grille2):
+    def comp(self,grille1,grille2):#on verifie si le complementaire est gagant 
         if grille1[5] == grille2[5]:
             return True
         else:
@@ -99,7 +99,7 @@ def gains(nombregagnant,nombrecomplementaire):
         print("ca fait 20 euros ca c'est pas mal")
         gains = gains + 20
         if nombrecomplementaire == True:
-            print("et avec le complementraire ca fait 10 balles !!")
+            print("et tu as aussi le complementaire quelle chance, 50 euros c'est pas mal !!")
             gains = gains +30
 
 
@@ -107,14 +107,14 @@ def gains(nombregagnant,nombrecomplementaire):
         print("ca fait beaucoup de fric tu es chanceux, tiens prend tes 400 balles ")
         gains = gains + 400
         if nombrecomplementaire == True:
-            print("et avec le complementraire ca fait 10 balles !!")
+            print("et le complementaire en plus, ce soir c'est champagne ! 1000 euros  !!")
             gains = gains + 600
 
     if nombregagnant == 5:
         print("que dire de plus tu as gagné au loto")
         gains = gains + 2000000
         if nombrecomplementaire == True:
-            print("et avec le complementraire ca fait 10 balles !!")
+            print("ca y est tu es riche il y a plus rien a ajouter  !!")
             gains = gains +1800000
 
     return gains
